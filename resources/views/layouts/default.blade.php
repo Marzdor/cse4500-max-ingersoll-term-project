7a388b24378b7d9f12c820ad0a63ea56837fb775
Max <marzdor@gmail.com>
2022-04-22 21:16:47 -0700

    added basic view layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+    @include('common.head')
+
+</head>
+
+<body>
+    <div class="container">
+
+
+        @yield('content')
+
+
+    </div>
+</body>
+
+</html>
